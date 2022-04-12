3afabed7591d2e108749646a7609a12f825b6031
dgana <dhegana@gmail.com>
2022-04-12 22:45:45 +0700

    add form UI in register

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Formik, Form } from 'formik'
+import { Wrapper } from '../components/Wrapper'
+import { InputField } from '../components/InputField'
+import { Button } from '@chakra-ui/react'
 
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
-  return <div>Register Page</div>
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <InputField
+              name="password"
+              placeholder="password"
+              label="Password"
+              type="password"
+            />
+            <Button
+              mt={4}
+              type="submit"
+              variant="solid"
+              isLoading={isSubmitting}
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  )
 }
 
 export default Register
