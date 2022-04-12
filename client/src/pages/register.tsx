3fec8442a83a0dfa86e6cd584c7ba30a70d11c22
dgana <dhegana@gmail.com>
2022-04-12 20:32:15 +0700

    add next-js starter template with chakra-ui and typescript

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface registerProps {
+
+}
+
+export const Register: React.FC<registerProps> = ({}) => {
+	return (
+		<div>Register Page</div>
+	);
+}
+
+export default Register
